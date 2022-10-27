f86be1390c1b1bdab24e908e603ddecfca08b3e2
bogdanbucur <bogdan.bucur1996@yahoo.com>
2022-10-27 15:40:09 +0300

    "uploading the ex1/day2"

---
+++ day2/ex1.py
@@ -0,0 +1,21 @@
+'''
+
+Let's learn about list comprehensions! You are given three integers  and  representing the
+dimensions of a cuboid along with an integer . Print a list of all possible coordinates given
+by  on a 3D grid where the sum of  is not equal to . Here, . Please use list comprehensions
+rather than multiple loops, as a learning exercise.
+
+'''
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+
+
+    list1 = [x, y, z]
+    list2 = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1)]
+    list3 = [i for i in list2 if sum(i) != n]
+    print(list3)
