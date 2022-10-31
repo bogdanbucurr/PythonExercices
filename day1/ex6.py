3e190b2f64140991f9b6330f8710eb6fc08cd86f
bogdanbucur <bogdan.bucur1996@yahoo.com>
2022-10-31 17:26:15 +0200

    "uploading the ex6/day1"

--- day1/ex6.py
+++ day1/ex6.py
@@ -1,7 +1,27 @@
 '''
 
-Given 2 sets of integers, M and N, print their symmetric difference in ascending order.
-The term symmetric difference indicates those values that exist in either M or N but do not
-exist in both.
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score.
+You are given 'n' scores. Store them in a list and find the score of the runner-up.
 
 '''
+
+if __name__ == '__main__':
+
+    print('Enter your inputs, please:')
+    #n = int(input())
+    arr = map(int, input().split())
+
+    # creating a list
+    l = list(arr)
+
+    # sorting list
+    l1 = sorted(l)
+
+    # using set() method to remove duplicated numbers
+    set1 = set(l1)
+
+    # making list from set to be able to get number from index
+    l2 = list(set1)
+
+    # getting runner up score from index
+    print(l2[-2])
